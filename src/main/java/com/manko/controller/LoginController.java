d05f1a03fe7ce6a3fa5f03ec89c58853da14be5c
smanko <smanko@playtika.com>
2017-06-27 19:13:37 +0300

    First very simple rest application

--- src/main/java/com/manko/controller/LoginController.java
+++ src/main/java/com/manko/controller/LoginController.java
@@ -53,12 +53,11 @@ public class LoginController {
 
     @RequestMapping(value="/admin/home", method = RequestMethod.GET)
     public ModelAndView home(){
-        ModelAndView modelAndView = new ModelAndView();
+        ModelAndView modelAndView = new ModelAndView("admin/home");
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findUserByEmail(auth.getName());
         modelAndView.addObject("userName", "Welcome " + user.getFirstName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
         modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
-        modelAndView.setViewName("admin/home");
         return modelAndView;
     }
 }
